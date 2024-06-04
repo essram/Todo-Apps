cd4a021bfcad72d0719cab83967baab10babf25d
essram <esarama22@gmail.com>
2024-06-04 08:30:49 +0700

    add js code

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const submitForm = document.getElementById('form');
+    submitForm .addEventListener('submit', function(event){
+        event.preventDefault();
+        addTodo();
+    })
+})
+
+function addTodo(){
+    const textTodo = document.getElementById('title').value;
+}
